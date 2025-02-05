b8cab2f3d65db16302f4221544f1e0b807bf8e93
Oliver England <oliver.england@dojo.tech>
2025-02-05 16:18:38 +0000

    Update refund-activity-message.kts

--- mobile-sdk/android/tap-to-pay/kotlin/refund-activity-message.kts
+++ mobile-sdk/android/tap-to-pay/kotlin/refund-activity-message.kts
@@ -1,3 +1,3 @@
 val transactionResult = data?.getStringExtra("com.dojo.extra.TRANSACTION_RESULT")
-val trxTransactionId = data?.getStringExtra("com.dojo.extra.TRANSACTION_ID")
+val TransactionId = data?.getStringExtra("com.dojo.extra.TRANSACTION_ID")
 val AuthCode = data?.getStringExtra("com.dojo.extra.AUTH_CODE")
