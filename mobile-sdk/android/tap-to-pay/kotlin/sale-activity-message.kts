9676678f94a9f8256e07a7a4565b60ba0e8f2151
Oliver England <oliver.england@dojo.tech>
2025-02-05 16:18:16 +0000

    Update sale-activity-message.kts

--- mobile-sdk/android/tap-to-pay/kotlin/sale-activity-message.kts
+++ mobile-sdk/android/tap-to-pay/kotlin/sale-activity-message.kts
@@ -1,3 +1,3 @@
-val trxResult = data?.getStringExtra("com.dojo.extra.TRANSACTION_RESULT", transactionResult: String/enum (SUCCESSFUL, DECLINED, CANCELLED, UNSUCCESSFUL))
-val trxTransactionId = data?.getStringExtra("com.dojo.extra.TRANSACTION_ID", transactionId: String)
-val trxAuthCode = data?.getStringExtra("com.dojo.extra.AUTH_CODE", authCode: String)
+val transactionResult = data?.getStringExtra("com.dojo.extra.TRANSACTION_RESULT")
+val TransactionId = data?.getStringExtra("com.dojo.extra.TRANSACTION_ID")
+val AuthCode = data?.getStringExtra("com.dojo.extra.AUTH_CODE")
