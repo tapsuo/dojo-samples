4df7544e70f4225b75d03c7054bac99c56d54c38
Oliver England <oliver.england@dojo.tech>
2025-02-06 14:46:48 +0000

    Update refund-response.kts

--- mobile-sdk/android/tap-to-pay/kotlin/refund-response.kts
+++ mobile-sdk/android/tap-to-pay/kotlin/refund-response.kts
@@ -1,6 +1,6 @@
 val intent = Intent("com.dojo.action.TX_COMPLETE")
 intent.putExtra("com.dojo.extra.REFUND_STATUS, refundStatus: String)
-intent.putExtra("com.dojo.extra.TRANSACTION_RESULT", transactionResult: String/enum (SUCCESSFUL, DECLINED, CANCELLED, UNSUCCESSFUL))
+intent.putExtra("com.dojo.extra.TRANSACTION_RESULT", transactionResult: String (enum: SUCCESSFUL, DECLINED, CANCELLED, UNSUCCESSFUL))
 intent.putExtra("com.dojo.extra.TRANSACTION_ID", transactionId: String)
 intent.putExtra("com.dojo.extra.AUTH_CODE", authCode: String)
 intent.putExtra("com.dojo.extra.BASE_AMOUNT", baseAmount: Integer)
